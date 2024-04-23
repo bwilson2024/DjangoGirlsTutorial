72f3a75bb5ee73994bd59149b1b7aaa296cd6921
bwilson2024 <brandonwilson2003@gmail.com>
2024-04-23 12:31:57 -0400

    Update models.py with additional classes

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from datetime import datetime
 
 
 class Post(models.Model):
@@ -16,3 +17,29 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+###########################
+#Add in additional classes for this app
+###########################
+
+class Person(models.Model):
+    WELLBEING_CHOICES = [
+        ('poor', 'Poor'),
+        ('average', 'Average'),
+        ('good', 'Good'),
+    ]
+    wellbeing = models.CharField(max_length=7, choices=WELLBEING_CHOICES, default='average')
+    # Other attributes like name, email, etc., could also be included here.
+
+class Meditation(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='meditations')
+    length = models.PositiveIntegerField(help_text="Duration in minutes")
+    impact = models.CharField(max_length=255, blank=True, help_text="Descriptive impact on wellbeing")
+
+class Journaling(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='journal_entries')
+    entry_number = models.PositiveIntegerField()
+    date = models.DateField(default=now)
+
+# Since WellbeingReport is not a database model but more of a service or utility for generating reports,
+# we don't define it as a Django model. Instead, report generation would be handled through views or a separate service layer.
