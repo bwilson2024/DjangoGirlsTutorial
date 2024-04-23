bc7caa1fadb19e839c63949a6d0ba938fb3cde2f
bwilson2024 <brandonwilson2003@gmail.com>
2024-04-23 12:36:10 -0400

    add in the registration of the new models in the built in admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Person,Meditation,Journaling
 
 admin.site.register(Post)
+admin.site.register(Person)
+admin.site.register(Meditation)
+admin.site.register(Journaling)
