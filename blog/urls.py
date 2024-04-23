d3b8d0cbcadda2a1f6568996ce25682d83357783
bwilson2024 <brandonwilson2003@gmail.com>
2024-04-23 14:49:10 -0400

    add url patterns for persons, meditations, journals

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 # add url pattern
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('persons/', views.person_list, name='person_list'),
+    path('meditations/', views.meditation_list, name='meditation_list'),
+    path('journalings/', views.journaling_list, name='journaling_list')
 ]
