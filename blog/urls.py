79b3d1e291366cc74c350655b91053168d51b409
bwilson2024 <brandonwilson2003@gmail.com>
2024-04-24 09:59:47 -0400

    add landing page dashboard including update to templates, urls and views

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('persons/', views.person_list, name='person_list'),
     path('meditations/', views.meditation_list, name='meditation_list'),
-    path('journals/', views.journaling_list, name='journaling_list')
+    path('journals/', views.journaling_list, name='journaling_list'),
+    path('dashboard/', views.dashboard, name='dashboard')
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -29,3 +29,39 @@ def journaling_list(request):
     journalings = Journaling.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # Pass the data to the template context
     return render(request, 'blog/journaling_list.html', {'journalings': journalings})
+
+###########################################
+#add in a homepage dashboard view
+###########################################
+
+# your_app/views.py
+
+from django.shortcuts import render
+from .models import Person, Meditation, Journaling
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        # Assuming the logged-in user is associated with a Person model
+        try:
+            person = Person.objects.get(user=request.user)
+            meditations = Meditation.objects.filter(person=person).order_by('-created_date')[:5]  # Last 5 sessions
+            journal_entries = Journaling.objects.filter(person=person).order_by('-date')[:5]  # Last 5 entries
+        except Person.DoesNotExist:
+            person = None
+            meditations = []
+            journal_entries = []
+        context = {
+            'person': person,
+            'meditations': meditations,
+            'journal_entries': journal_entries,
+            'greeting': f"Welcome back, {request.user.first_name}!"
+        }
+    else:
+        # Default context for non-logged in users
+        context = {
+            'person': None,
+            'meditations': [],
+            'journal_entries': [],
+            'greeting': "Welcome to Wellbeing for Youth!"
+        }
+    return render(request, 'blog/dashboard.html', context)
