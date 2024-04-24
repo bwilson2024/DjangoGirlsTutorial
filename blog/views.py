71d1be54d6da425d5fa8d8d9dc2f375fb617639d
bwilson2024 <brandonwilson2003@gmail.com>
2024-04-24 15:02:58 -0400

    update table view on wellbeing report

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post, Person, Meditation, Journaling
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 def post_list(request):
     #run the query set and
